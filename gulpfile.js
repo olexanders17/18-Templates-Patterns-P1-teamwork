ecaaec29b44e729cfcdb11899ec178cdde479933
olexanders <aaa@ss.ty>
2016-09-07 11:48:52 +0000

    upd gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,32 @@
-var gulp=require("gulp");
+var gulp = require("gulp");
+var eslint = require('gulp-eslint');
+var jade = require('gulp-jade');
+
+
+gulp.task('copy',function () {
+   gulp.src("./src/lection.html")
+       .pipe(gulp.dest('./dist/'))
+
+});
+
+
+gulp.task('eslint', function () {
+    return gulp.src(['src/**/*.js', ])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+
+
+
+gulp.task('jade', function() {
+    var YOUR_LOCALS = {};
+
+    gulp.src('./src/*.jade')
+        .pipe(jade({
+            locals: YOUR_LOCALS
+        }))
+        .pipe(gulp.dest('./dist/'))
+});
 
